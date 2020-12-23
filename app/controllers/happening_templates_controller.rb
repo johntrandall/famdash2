013ae19954b8aeb4b9a212df33bf2edeb25150f2
John Randall <john@johnrandall.com>
2020-12-23 02:16:15 -0500

    user can edit happening templates

--- app/controllers/happening_templates_controller.rb
+++ app/controllers/happening_templates_controller.rb
@@ -1,6 +1,24 @@
 class HappeningTemplatesController < ApplicationController
   def index
-    (redirect_back(fallback_location: root_path) and return) if selected_user.nil?
+    if selected_user.nil? || current_user.nil?
+      redirect_back(fallback_location: root_path)
+      return
+    end
     @happening_templates = selected_user.happening_templates
+  end
+
+  def update
+    happening_template = selected_user.happening_templates.find(params[:id])
+    # TODO - permissions
+    happening_template.update!(happening_template_params)
+    flash[:success] = 'Habit updated'
+
+    redirect_back(fallback_location: happening_templates_path)
+  end
+
+  private
+
+  def happening_template_params
+    params.require(:happening_template).to_unsafe_hash #TODO
   end
 end
--- spec/controllers/happening_templates_controller_spec.rb
+++ spec/controllers/happening_templates_controller_spec.rb
@@ -1,8 +1,32 @@
 require 'rails_helper'
 
 describe HappeningTemplatesController do
+
   describe '#index' do
-    context 'without a selected user' do
+
+    let(:user) { User.create! }
+    let(:selected_user) { user }
+    let(:current_user) { user }
+
+    before do
+      allow(session).to receive(:[]).with('flash').and_call_original
+      allow(session).to receive(:[]).with(:_turbolinks_location).and_call_original
+
+      allow(session).to receive(:[]).with(:selected_user_id).and_return(selected_user)
+      allow(session).to receive(:[]).with(:current_user_id).and_return(current_user)
+    end
+
+    context 'without a current_user' do
+      let(:current_user) { nil }
+
+      it 'redirects' do
+        get :index
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context 'without a selected_user' do
+      let(:selected_user) { nil }
       it 'redirects' do
         get :index
         expect(response).to redirect_to(root_path)
@@ -10,14 +34,18 @@ describe HappeningTemplatesController do
     end
 
     it "shows the selected-user's happening_templates" do
-      user = User.create!
       happening_template = HappeningTemplate.create!
-      happening_template.users << user
+      user.happening_templates << happening_template
 
-      expect_any_instance_of(described_class).to receive(:selected_user).and_return(user)
+      # expect_any_instance_of(described_class).to receive(:selected_user).and_return(user)
+      # expect_any_instance_of(described_class).to receive(:current_user).and_return(user)
+
+      # allow(session).to receive('flash').and_call_original
+
+      name
 
       get :index
-      expect(response.body).to include(happening_template.description)
+      expect(response).to be_successful
     end
   end
 end
