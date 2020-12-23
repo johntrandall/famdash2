51e3f5da7820f3b5c86755e9a466ad2772194339
John Randall <john@johnrandall.com>
2020-12-23 01:24:09 -0500

    fix-spec Happenings Controller

--- spec/controllers/happenings_controller_spec.rb
+++ spec/controllers/happenings_controller_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 describe HappeningsController do
   describe '#create' do
     subject do
-      template.users << happening_user
+      template.users << reportee_user
       # expect_any_instance_of(ApplicationController).to receive(:current_user).and_return(reporting_user)
-      post :create, params: { user_id: reportee_user.id, reporting_user_id: reporting_user.id, template_id: template.id }
+      post :create, params: { selected_user_id: reportee_user.id, reporting_user_id: reporting_user.id, template_id: template.id }
     end
 
     let(:template) { HappeningTemplate.create!(point_value: 99) }
@@ -19,7 +19,7 @@ describe HappeningsController do
     end
 
     it 'creates a happening from a happening template' do
-      template.users << happening_user
+      template.users << reportee_user
 
       expect { subject }.to change { Happening.count }.by 1
       expect(Happening.last.point_value).to eq template.point_value
