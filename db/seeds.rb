3b322ca41b2fff725263dd46e098fd2aae730348
John Randall <john@johnrandall.com>
2020-12-26 01:01:50 -0500

    seperators

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,6 +34,10 @@ end
 
 User.child.each do |child_user|
   # user.happening_templates.destroy_all!
+  child_user.happening_templates.find_or_create_by!(kind: :separator,
+                                                    description: 'morning',
+                                                    point_value: 0)
+
   child_user.happening_templates.find_or_create_by!(kind: :good_habit,
                                                     description: 'Waking up on time',
                                                     point_value: 1)
@@ -48,6 +52,9 @@ User.child.each do |child_user|
                                                     description: 'In the morning, remembered to brush',
                                                     point_value: 5)
 
+  child_user.happening_templates.find_or_create_by!(kind: :separator,
+                                                    description: 'mealtime',
+                                                    point_value: 0)
   child_user.happening_templates.find_or_create_by!(kind: :pass_fail_habit,
                                                     description: 'before eating, remembered to wash hands',
                                                     point_value: 1)
@@ -68,6 +75,9 @@ User.child.each do |child_user|
                                                     description: 'Finshed everyhthing after dinner in < 15min',
                                                     point_value: 10)
 
+  child_user.happening_templates.find_or_create_by!(kind: :separator,
+                                                    description: 'evening',
+                                                    point_value: 0)
   child_user.happening_templates.find_or_create_by!(kind: :pass_fail_habit,
                                                     description: 'before bed, remembered to floss and brush at night',
                                                     point_value: 5)
@@ -107,15 +117,15 @@ User.find_by(display_name: 'Sam').happening_templates.find_or_create_by!(kind: :
 User.find_by(display_name: 'Finn').happening_templates.find_or_create_by!(kind: :pass_fail_habit,
                                                                           description: 'Cleaning up quickly at cleanup time').update(point_value: 5)
 
-User.where(display_name: 'TestChild').destroy_all
-test_child = User.create!({ display_name: 'TestChild',
-                            role: :child })
-test_child.happenings.create!(reporting_user: test_child,
-                              kind: :good_habit,
-                              point_value: 5,
-                              description: 'i am a happening description')
-happening_template = HappeningTemplate.where(kind: :good_habit,
-                                             point_value: 5).first_or_create!
-happening_template.description ||= 'remembered to wash hands before eating'
-happening_template.save!
-happening_template.users << test_child
+# User.where(display_name: 'TestChild').destroy_all
+# test_child = User.create!({ display_name: 'TestChild',
+#                             role: :child })
+# test_child.happenings.create!(reporting_user: test_child,
+#                               kind: :good_habit,
+#                               point_value: 5,
+#                               description: 'i am a happening description')
+# happening_template = HappeningTemplate.where(kind: :good_habit,
+#                                              point_value: 5).first_or_create!
+# happening_template.description ||= 'remembered to wash hands before eating'
+# happening_template.save!
+# happening_template.users << test_child
