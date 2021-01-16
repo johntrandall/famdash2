cc5268252304be18ba429462f4ebce535246127f
John Randall <john@johnrandall.com>
2021-01-15 21:11:33 -0500

    datagrid column order

--- app/grids/happenings_grid.rb
+++ app/grids/happenings_grid.rb
@@ -7,6 +7,7 @@ class HappeningsGrid < BaseGrid
   # filter(:id, :integer)
   # filter(:created_at, :date, :range => true)
 
+  date_column(:created_at)
   column(:id)
   column(:name)
   column(:description)
@@ -14,5 +15,4 @@ class HappeningsGrid < BaseGrid
   column(:point_value)
   column(:happening_template_id, :header => "Template ID")
   column(:happening_template, :header => "Template Name") { |temp| temp.name }
-  date_column(:created_at)
 end
