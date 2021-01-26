e1e3800fa656a6deee40d3514537afc116054b1e
John Randall <john@johnrandall.com>
2021-01-26 08:37:52 -0500

    cleanup of happenings logs (#7)

--- app/models/happening/decay_factory.rb
+++ app/models/happening/decay_factory.rb
@@ -11,21 +11,22 @@ class Happening
 
       array_of_last_7_days.each do |date|
         sucess_score_to_date = user.happenings.where(event_kind: :habit_success, reported_at: [...date.beginning_of_day])
-                                  .sum(:point_value)
+                                   .sum(:point_value)
         success_score_change = -1 * (sucess_score_to_date / 3)
 
         if success_score_change.nonzero?
           decay_happening = user.happenings.find_or_create_by!(event_kind: :habit_success,
-                                                          decay_event: true,
-                                                          reported_at: date.beginning_of_day)
+                                                               decay_event: true,
+                                                               reported_at: date.beginning_of_day)
           if decay_happening.point_value != success_score_change
-            decay_happening.update!(description: 'success score overnight decay (1/3)',
+            decay_happening.update!(name: 'success score fade',
+                                    description: 'success score overnight fade - loose (1/3)',
                                     point_value: success_score_change)
           end
         end
 
         fail_score_to_date = user.happenings.where(event_kind: :habit_fail, reported_at: [...date.beginning_of_day])
-                                   .sum(:point_value)
+                                 .sum(:point_value)
         fail_score_change = -1 * (fail_score_to_date / 3)
 
         if fail_score_change.nonzero?
@@ -33,7 +34,8 @@ class Happening
                                                                decay_event: true,
                                                                reported_at: date.beginning_of_day)
           if decay_happening.point_value != fail_score_change
-            decay_happening.update!(description: 'fail score overnight forgiveness',
+            decay_happening.update!(name: 'fail score heal',
+                                    description: 'fail score overnight forgiveness - loose 1/3',
                                     point_value: fail_score_change)
           end
         end
