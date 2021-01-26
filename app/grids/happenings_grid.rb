07d03c335b75592912383e7e85892d43a8e00021
John Randall <john@johnrandall.com>
2021-01-25 22:41:42 -0500

    cleanup of happenings logs

--- app/grids/happenings_grid.rb
+++ app/grids/happenings_grid.rb
@@ -8,13 +8,33 @@ class HappeningsGrid < BaseGrid
   # filter(:created_at, :date, :range => true)
 
   date_column(:created_at)
-  column(:id)
+  # column(:event_kind)
+  column(:event_kind) do |happening|
+    if happening.habit_success?
+      "✅"
+    elsif happening.habit_fail?
+      "❌"
+    end
+  end
+  column(:decay_event, header: "heal?") do |happening|
+    if happening.decay_event
+      if happening.habit_fail?
+        "🛌 💗"
+      elsif happening.habit_success?
+        "🛌 💗"
+      end
+    end
+  end
+  # column(:id)
+  column(:point_value)
   column(:name)
   column(:description)
-  column(:event_kind)
-  column(:point_value)
-  column(:happening_template_id, :header => "Template ID")
-  column(:happening_template, :header => "Template Name") { |temp| temp.name }
+  # column(:happening_template_id, :header => "Template ID")
+  column(:happening_template, :header => "Template")  do |teamplate|
+    ActionController::Base.helpers.link_to_if( teamplate.id.present?,
+                                               teamplate.name || teamplate.description,
+                                               Rails.application.routes.url_helpers.happening_templates_path(anchor: "happeningTemplate-#{teamplate.id}"))
+  end
   column(:id) do |happening|
     ActionController::Base.helpers.link_to('edit', Rails.application.routes.url_helpers.edit_happening_path(happening))
   end
