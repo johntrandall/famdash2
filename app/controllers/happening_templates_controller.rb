1a8d8ef5cdc21d9ded10f60fb05b4837190294f9
John Randall <john@johnrandall.com>
2021-02-01 19:13:08 -0500

    cache the image

--- app/controllers/happening_templates_controller.rb
+++ app/controllers/happening_templates_controller.rb
@@ -9,29 +9,7 @@ class HappeningTemplatesController < ApplicationController
 
   def show
     @happening_template = selected_user.happening_templates.find(params[:id])
-
-    array_of_dates = (14.days.ago.to_date...Time.current.to_date).uniq
-    require 'gruff'
-    g = Gruff::Bar.new
-    g.title = @happening_template.name
-    g.labels = array_of_dates
-                 .each_with_index.map { |x, i| [i, x] }.to_h
-                 .select { |k, v| v.strftime('%A') == "Monday" }
-                 .transform_values.each { |v| v = v&.strftime('%A, %m/%d') }
-    g.data(:successes,
-           array_of_dates
-             .map { |date| @happening_template.happenings.habit_success.where(reported_at: [date.beginning_of_day...date.end_of_day]).count },
-           '#33cc33')
-    g.data(:fails,
-           array_of_dates
-             .map { |date| @happening_template.happenings.habit_fail.where(reported_at: [date.beginning_of_day...date.end_of_day]).count * -1 },
-           '#ff0000')
-    g.y_axis_increment = 1
-    g.marker_font_size = 10
-    g.no_data_message = 'no data'
-
-    @file_name = "gruff/#{controller_name}_#{action_name}_#{@happening_template.id}.png"
-    g.write("app/assets/images/#{@file_name}")
+    @file_name = generate_graph_filename
   end
 
   def create
@@ -75,6 +53,42 @@ class HappeningTemplatesController < ApplicationController
   end
 
   private
+
+  def generate_graph_filename
+    file_name = "gruff/#{controller_name}_#{action_name}_#{@happening_template.id}.png"
+    cache_key = "#{file_name}-#{@happening_template.happenings.minimum(:updated_at).to_i}"
+    Rails.cache.fetch(cache_key) do
+      write_graph_image(file_name)
+    end
+    file_name
+  end
+
+  def write_graph_image(file_name)
+    array_of_dates = (14.days.ago.to_date...Time.current.to_date).uniq
+    require 'gruff'
+    g = Gruff::Bar.new
+    g.title = @happening_template.name
+    g.labels = array_of_dates
+                 .each_with_index.map { |x, i| [i, x] }.to_h
+                 .select { |k, v| v.strftime('%A') == "Monday" }
+                 .transform_values.each { |v| v = v&.strftime('%A, %m/%d') }
+    g.data(:successes,
+           array_of_dates
+             .map { |date| happening_template_happenings_habit_success_where = @happening_template.happenings.habit_success.where(reported_at: [date.beginning_of_day...date.end_of_day])
+             happening_template_happenings_habit_success_where.count },
+           '#33cc33')
+    g.data(:fails,
+           array_of_dates
+             .map { |date| @happening_template.happenings.habit_fail.where(reported_at: [date.beginning_of_day...date.end_of_day]).count * -1 },
+           '#ff0000')
+    g.y_axis_increment = 1
+    g.marker_font_size = 10
+    g.no_data_message = 'no data'
+
+    g.write("app/assets/images/#{file_name}")
+
+    file_name
+  end
 
   def happening_template_params
     params.require(:happening_template).permit(:name,
